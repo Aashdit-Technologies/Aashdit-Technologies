a99b706f8cf7139d1ca31226e53a58d532ff43fa
Ajay Kumar Manasingh <ajay.manasingh@aashdit@com>
2024-10-25 11:41:40 +0530

    Add all React project files

--- src/App.jsx
+++ src/App.jsx
@@ -3,22 +3,22 @@ import "bootstrap/dist/js/bootstrap.bundle.min";
 import "./App.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import DashboardAll from "./components/dashboardall/DashboardAll"
-// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-// import Sidebar from "./components/sidebar/Sidebar";
-// import ForecastLogin from "./components/ForecastLogin";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Sidebar from "./components/sidebar/Sidebar";
+import ForecastLogin from "./components/ForecastLogin";
 
 const queryClient = new QueryClient();
 
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <DashboardAll/>
-      {/* <Router>
+      {/* <DashboardAll/> */}
+      <Router>
         <Routes>
           <Route path="/" element={<ForecastLogin />} />
           <Route path="/dashboard" element={<Sidebar />} />
         </Routes>
-      </Router> */}
+      </Router>
     </QueryClientProvider>
   );
 }
--- src/components/authService.js
+++ src/components/authService.js
@@ -1,7 +1,7 @@
 
 import axios from "axios";
 
-const API_URL = "http://157.245.99.116:8080/sanrakhshana/api/login";
+const API_URL = "http://192.168.3.121:1010/hrms/api/login";
 
 export const loginUser = async (userName, password) => {
   try {
